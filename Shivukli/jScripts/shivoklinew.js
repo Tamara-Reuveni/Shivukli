1d069f7186774de2372575e3ca23cbd36a3635ef
Tamara-Reuveni <tama95@gmail.com>
2017-07-05 13:47:24 +0300

    Update shivoklinew.js

--- Shivukli/jScripts/shivoklinew.js
+++ Shivukli/jScripts/shivoklinew.js
@@ -1370,7 +1370,7 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/shivoklinew_atlas_.png", id:"shivoklinew_atlas_"}
+		{src:"../images/shivoklinew_atlas_.png", id:"shivoklinew_atlas_"}
 	],
 	preloads: []
 };
